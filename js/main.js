bbd78ab3ba8f30486c11c2b29faf0adaf7154a05
tullyt <tullyt@seattleu.edu>
2017-11-04 01:02:02 -0400

    My Image Maker

--- js/main.js
+++ js/main.js
@@ -1,96 +1,85 @@
-/* WATS 3020 Image Maker Code */
-
-//////////////////////////////////////////////////
-// ImageMaker Class Definition               /////
-////////////////////////////////////////////////////////////////////////
-// This class is used to manage the image manipulation and prep on    //
-// the page. It is instantiated when the page is loaded, and it       //
-// handles all the logic of updating the image preview with the data  //
-// submitted by users via the image maker form.                       //
-////////////////////////////////////////////////////////////////////////
-
 class ImageMaker {
-    constructor(){
-        // When this class is instantiated, the `constructor()` method is executed.
-        // TODO: Set up attributes that point to the HTML elements we wish to work with.
+  constructor() {
 
-        // TODO: Select the `#image-preview` div using any document selector method.
-        this.imagePreview = ;
+    // Point attributes to point at HTML elements
+    // TODO: Select the `#image-preview` div using any document selector method.
+    this.imagePreview = document.getElementById('image-preview');
 
-        // TODO: create a new `<p>` element called `this.topText`
-        this.topText = ;
-        // TODO: Add a `class` attribute to `this.topText` that contains the classname "top-text".
-
-        // TODO: Append `this.topText` as a child element to `this.imagePreview`
+    // New 'p' and element called 'this.topText'
+    this.topText = document.createElement('p');
 
 
-        // TODO: create a new `<p>` element called `this.bottomText`
-        this.bottomText = ;
-        // TODO: Add a `class` attribute to `this.bottomText` that contains the classname "bottom-text".
+    // Added a `class` attribute to `this.topText` that contains the classname "top-text".
+    this.topText.setAttribute('class', 'top-text');
 
-        // TODO: Append `this.bottomText` as a child element to `this.imagePreview`
+    // Added `this.topText` as a child element to `this.imagePreview`
+    this.imagePreview.appendChild(this.topText);
 
-        // This class also needs to use the form fields to read user input. Set
-        // those up for future use, too.
+    // Created another new `<p>` element called `this.bottomText`
+    this.bottomText = document.createElement('p');
 
-        // TODO: Select the `input` element with the `name` attribute "backgroundImage"
-        this.backgroundInput = ;
+    // Added a `class` attribute to `this.bottomText` that contains the classname "bottom-text".
+    this.bottomText.setAttribute('class', 'bottom-text');
 
-        // TODO: Select the `input` element with the `name` attribute "topText"
-        this.topTextInput = ;
+    // Added `this.bottomText` as a child element to `this.imagePreview`
+    this.imagePreview.appendChild(this.bottomText);
 
-        // TODO: Select the `input` element with the `name` attribute "bottomText"
-        this.bottomTextInput = ;
+    // Select `input` element with the `name` attribute "backgroundImage"
+    this.backgroundInput = document.querySelector('select[name="backgroundImage"]');
 
-        // NOTE: If you add additional form fields to modify other aspects of
-        // the image, then you will need to make attributes for each of those
-        // elements here.
-    }
-    drawPreview(){
-        // This function is called whenever a user changes one of the form fields
-        // and whenever an image is generated for download. This function must
-        // update the style attributes and innerHTML content of the HTML
-        // elements selected in the `constructor()` of this class in order to
-        // update `this.imagePreview`.
+    // Select  `input` element with the `name` attribute "topText"
+    this.topTextInput = document.querySelector('input[name="topText"]');
 
-        // TODO: Update the `background-image` CSS property for `this.imagePreview`.
+    //  Select  `input` element with the `name` attribute "bottomText"
+    this.bottomTextInput = document.querySelector('input[name="bottomText"]');
 
-        // TODO: Update the `innerHTML` of `this.topText`.
+    //Images
+  }
+  drawPreview() {
+    // This function is called whenever a user changes one of the form fields
+    // and whenever an image is generated for download. This function must
+    // update the style attributes and innerHTML content of the HTML
+    // elements selected in the `constructor()` of this class in order to
+    // update `this.imagePreview`.
 
-        // TODO: Update the `innerHTML` of `this.bottomText`
+    // CSS: Updated the `background-image` CSS property for `this.imagePreview`.
+    this.imagePreview.style.backgroundImage = `url(images/${this.backgroundInput.value})`;
 
+    // InnerHTML: Updatde the `innerHTML` of `this.topText`.
+    this.topText.innerHTML = this.topTextInput.value;
 
-    }
-    downloadImage(){
-        this.drawPreview();
-        generateImage();
-    }
+    // InnerHTML: Updated the `innerHTML` of `this.bottomText`
+    this.bottomText.innerHTML = this.bottomTextInput.value;
+
+  }
+  downloadImage() {
+    this.drawPreview();
+    generateImage();
+  }
 }
 
 let imageMaker = new ImageMaker();
-
-//////////////////////////////////////////////////
-// Do Not Edit Below This Line               /////
-////////////////////////////////////////////////////////////////////////
 
 // This function uses the `domtoimage` module to render an image of the
 // `#image-preview` element and prompts the user to download the created image.
 // It is possible to use the `height` and `width` parameters to alter the size
 // of the rendered image.
-function generateImage(elementID="image-preview", height="800px", width="1280px"){
-    let htmlTemplate = document.getElementById(elementID);
-    htmlTemplate.style.height = height;
-    htmlTemplate.style.width = width;
-    let imageName = "image_" + Date.now();
+function generateImage(elementID = "image-preview", height = "800px", width = "1280px") {
+  let htmlTemplate = document.getElementById(elementID);
+  htmlTemplate.style.height = height;
+  htmlTemplate.style.width = width;
+  let imageName = "image_" + Date.now();
 
-    // Generate image and prompt download for user.
-    domtoimage.toJpeg(htmlTemplate, { quality: 0.95 })
-        .then(function (dataUrl) {
-            var link = document.createElement('a');
-            link.download = imageName;
-            link.href = dataUrl;
-            link.click();
-        });
+  // Generate image and prompt download for user.
+  domtoimage.toJpeg(htmlTemplate, {
+      quality: 0.95
+    })
+    .then(function(dataUrl) {
+      var link = document.createElement('a');
+      link.download = imageName;
+      link.href = dataUrl;
+      link.click();
+    });
 }
 
 
@@ -104,18 +93,18 @@ function generateImage(elementID="image-preview", height="800px", width="1280px"
 //
 // The submit listener on the form interrupts the regular form processing of the
 // browser and calls the `imageMaker.downloadImage()` method.
-function applyEventListeners(){
-    let inputs = document.querySelectorAll('input, select, textarea');
-    for (input of inputs){
-        input.addEventListener("change", function(event){
-            imageMaker.drawPreview();
-        })
-    }
-    let imageForm = document.querySelector('form');
-    imageForm.addEventListener('submit', function(event){
-        event.preventDefault();
-        imageMaker.downloadImage();
+function applyEventListeners() {
+  let inputs = document.querySelectorAll('input, select, textarea');
+  for (input of inputs) {
+    input.addEventListener("change", function(event) {
+      imageMaker.drawPreview();
     })
+  }
+  let imageForm = document.querySelector('form');
+  imageForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    imageMaker.downloadImage();
+  })
 }
 
 // Apply event listeners on page load.
